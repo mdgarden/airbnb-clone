29b4bbe4e6c3d2dfe910957d228e44bed8300023
mdgarden <chooi9522@gmail.com>
2020-09-22 01:00:22 +0900

    #5.2 Lists Model

---
+++ users/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+# Register your models here.
+# user model을 admin패널에서 보고 싶다
+# 데코레이터가admin.site.register(models.User, CustomUserAdmin) 과 같은 뜻
+@admin.register(models.User)  # decorator는 이렇게 클래스 위에 있어야 작동함
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
