63d7a2e4e873cbbde0c4a55f14e5934f5162e56f
mdgarden <chooi9522@gmail.com>
2020-09-22 01:20:55 +0900

    #5.3 Conversations and Messages Models

--- lists/models.py
+++ lists/models.py
@@ -7,7 +7,7 @@ class List(core_models.TimeStampedModel):
 
     name = models.CharField(max_length=80)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    rooms = models.ManyToManyField("rooms.Room", blank=Ture)
+    rooms = models.ManyToManyField("rooms.Room", blank=True)
 
     def __str__(self):
         return self.name
