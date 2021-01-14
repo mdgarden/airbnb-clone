bfe0dcf69c8faee0efd419aa39035d3b69ccb812
mdgarden <chooi9522@gmail.com>
2021-01-14 21:05:52 +0900

    #10.3 Extending Templates part One

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from datetime import datetime
+from . import models
 
 
 # Create your views here.
 def all_rooms(request):
-    now = datetime.now()
-    hungry = True
-    return render(request, "all_rooms.html", context={
-        'now':now,
-        'hungry':hungry
-    })
+    all_rooms = models.Room.objects.all()
+    return render(request, "home.html", context={"rooms": all_rooms})
