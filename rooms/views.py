771a9387fda9fbd266d4420e05d223ceb4b0869c
mdgarden <chooi9522@gmail.com>
2021-03-01 20:04:27 +0900

    inspec for continue

--- rooms/views.py
+++ rooms/views.py
@@ -3,6 +3,12 @@ from . import models
 
 
 # Create your views here.
+
+
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    return render(request, "rooms/home.html", {"potato": all_rooms})
