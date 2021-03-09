4597c68a462baf5f82465ba8e0d6cd0afa5d486c
mdgarden <chooi9522@gmail.com>
2021-03-10 00:41:40 +0900

    #13.0 Templage, Form, Url, Setup

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -24,5 +25,12 @@ class HomeView(ListView):
 
 class RoomDetail(DetailView):
 
-    """ RoomDetail Definition """"
+    """ RoomDetail Definition """
+
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
