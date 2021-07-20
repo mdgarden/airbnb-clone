1198c7766971aee9450dd455c721ca27a03ab02c
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-07-20 21:23:46 +0900

    #20.10 Sign up

--- users/views.py
+++ users/views.py
@@ -32,7 +32,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {"first_name": "Tuple", "last_name": "Cardinality", "email": "email@email.com"}
 
     def form_valid(self, form):
         form.save()
