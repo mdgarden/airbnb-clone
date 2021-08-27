7a28ab99ecde98067fd16a7b5275ac8dd9820a73
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-08-27 23:41:31 +0900

    24.5 BookedDays part One

--- reservations/admin.py
+++ reservations/admin.py
@@ -17,3 +17,7 @@ class ReservationAdmin(admin.ModelAdmin):
     )
 
     list_filter = ("status",)
+
+@admin.register(models.BookedDay)
+class BookedDayAdmin(admin.ModelAdmin):
+    pass
--- reservations/models.py
+++ reservations/models.py
@@ -5,6 +5,16 @@ from core import models as core_models
 # Create your models here.
 
 
+class BookedDay(core_models.TimeStampedModel):
+
+    day = models.DateField()
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Booked Day"
+        verbose_name_plural = "Booked Days"
+
+
 class Reservation(core_models.TimeStampedModel):
 
     """ Reservation Definition """
@@ -41,3 +51,14 @@ class Reservation(core_models.TimeStampedModel):
         return now > self.check_out
 
     is_finished.boolean = True
+
+    def save(self, *args, **kwargs):
+            if True:
+                start = self.check_in
+                end = self.check_out
+                difference = end - start
+                existing_booked_day = BookedDay.objects.filter(
+                    day__range=(start, end)
+                ).exists()
+
+            return super().save(*args, **kwargs)
