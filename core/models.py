54307158ae2f9aef764d01bea820c4c3c82227e7
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-09-05 03:51:45 +0900

    24.15 Reviewing Rooms part Two

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from . import managers
 
 
 class TimeStampedModel(models.Model):
@@ -7,6 +8,8 @@ class TimeStampedModel(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomModelManager()
+
 
     class Meta:
         abstract = True  # abstract 모델 : DB에 나타나지않는 모델
--- reservations/models.py
+++ reservations/models.py
@@ -2,7 +2,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
-from . import managers
 
 # Create your models here.
 
@@ -41,7 +40,6 @@ class Reservation(core_models.TimeStampedModel):
     check_out = models.DateField()
     guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
-    objects = managers.CustomReservationManager()
 
     def __str__(self):
         return f"{self.room} - {self.check_in}"
