b8ebad154910b171138914f7327786f9b9c0d562
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-09-07 07:06:55 +0900

    25.2 Changing Language

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django.utils import translation
 from django.http import HttpResponse
 from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
@@ -209,8 +210,9 @@ def switch_hosting(request):
         request.session["is_hosting"] = True
     return redirect(reverse("core:home"))
 
+
 def switch_language(request):
     lang = request.GET.get("lang", None)
     if lang is not None:
-        pass
+        request.session[translation.LANGUAGE_SESSION_KEY] = lang
     return HttpResponse(status=200)
